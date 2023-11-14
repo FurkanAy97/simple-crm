8a0dc4039c189fab4a5c85c6cff6cafc33036e33
FurkanAyhan <109309647+FurkanAy97@users.noreply.github.com>
2023-11-14 15:18:14 +0100

    implement add user in firebase + add loading bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { FormsModule } from '@angular/forms';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 
 
@@ -33,7 +34,7 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 @NgModule({
   declarations: [AppComponent, DashboardComponent, UserComponent, DialogAddUserComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatFormFieldModule, MatDatepickerModule,
-    MatButtonModule, MatTooltipModule, MatDialogModule, MatInputModule, MatNativeDateModule, FormsModule, provideFirebaseApp(() => initializeApp({"projectId":"simple-crm-be31e","appId":"1:422824591526:web:9d875653e5134a49d37467","storageBucket":"simple-crm-be31e.appspot.com","apiKey":"AIzaSyC1r3CD_-FiHBZc4YPxGIbbpbgdOFhunSs","authDomain":"simple-crm-be31e.firebaseapp.com","messagingSenderId":"422824591526"})), provideFirestore(() => getFirestore())],
+    MatButtonModule, MatTooltipModule, MatDialogModule, MatInputModule, MatNativeDateModule, MatProgressBarModule, FormsModule, provideFirebaseApp(() => initializeApp({"projectId":"simple-crm-be31e","appId":"1:422824591526:web:9d875653e5134a49d37467","storageBucket":"simple-crm-be31e.appspot.com","apiKey":"AIzaSyC1r3CD_-FiHBZc4YPxGIbbpbgdOFhunSs","authDomain":"simple-crm-be31e.firebaseapp.com","messagingSenderId":"422824591526"})), provideFirestore(() => getFirestore())],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -12,21 +12,48 @@ import { Observable } from 'rxjs';
   styleUrls: ['./dialog-add-user.component.scss']
 })
 export class DialogAddUserComponent {
-  
+
   user = new User();
   birthDate: any;
   firestore: Firestore = inject(Firestore)
   users$: Observable<any[]>;
-
+  loading: boolean = false
 
   constructor() {
     const userCollection = collection(this.firestore, 'users')
     this.users$ = collectionData(userCollection);
   }
-  
+
   async saveUser() {
-    this.user.birthDate = this.birthDate.getTime();
-    await setDoc(doc(this.firestore, 'users', 'j2X9lKe3PDRtCkN7zw5N'),  JSON.parse(JSON.stringify(this.user)));
+    this.loading = true;
+    try {
+      this.user.birthDate = this.birthDate.getTime();
+
+      // Assuming this.firestore is your Firestore instance
+      const userRef = await addDoc(collection(this.firestore, 'users'), JSON.parse(JSON.stringify(this.user)));
+
+      // Continue your logic here after the user is successfully added
+      console.log('User added successfully with ID:', userRef.id);
+      this.emptyFields()
+      
+      // Additional logic can be placed here
+
+    } catch (error) {
+      // Handle any errors that occurred during the asynchronous operation
+      console.error('Error adding user:', error);
+
+    } finally {
+      this.loading = false;
+    }
+  }
+
+  emptyFields() {
+    this.user.firstName = '';
+    this.user.lastName = '';
+    this.birthDate = '';
+    this.user.street = '';
+    this.user.zipCode = undefined;
+    this.user.city = '';
   }
 
 }
